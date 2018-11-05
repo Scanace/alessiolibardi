725c37d1b34b85bdf57c711c5460bc91f7166c91
Alessio Libardi <alessiolibardi@gmail.com>
2018-11-05 20:36:08 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(window).on('load', function(e){
+  $(this).on('click', function(e){
+    alert("This is just a design sketch. While waiting for the final website release, contact me at 'alessiolibardi@gmail.com'");
+  });
+});
+
+$(document).ready(function(){
+
+});
